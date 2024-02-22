67d26eb53a3807a1a261dfca2f110bc91ff4df77
oprifine <oprifine4t@hotmail.com>
2024-02-22 18:54:16 -0500

    installer upgrade

--- databases/installer.py
+++ databases/installer.py
@@ -1,7 +1,7 @@
 import subprocess as sp
 
 print("Welcome to the flash package installer!")
-mng = input("What manager do you use? \n"
+mng = input("What manager do you use?\n"
              "|apt| Linux package manager.\n"
              "|yum| Linux package manager.\n"
              "|winget| Windows package manager.\n"
@@ -12,14 +12,20 @@ pkg = input("What package do you want to install/uninstall? ")
 # Ensure the user input is lowercase for case-insensitive comparison
 mng = mng.lower()
 
+# Prompt the user for flags
+use_flags = input("Do you want to use flags? (yes/no): ").lower()
+flags = ""
+if use_flags == "yes":
+    flags = input("Enter flags (separated by space): ")
+
 if mng == "apt":
-    command = f"sudo apt install {pkg}"
+    command = f"sudo apt install {pkg} {flags}"
 elif mng == "yum":
-    command = f"sudo yum install {pkg}"
+    command = f"sudo yum install {pkg} {flags}"
 elif mng == "winget":
-    command = f"winget install {pkg}"
+    command = f"winget install {pkg} {flags}"
 elif mng == "pip":
-    command = f"pip install {pkg}"
+    command = f"pip install {pkg} {flags}"
 elif mng == "exit":
     print("Exiting the manager.")
     exit()
